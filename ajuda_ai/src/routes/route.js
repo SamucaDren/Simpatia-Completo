63a3f75a7ee3529d1b260b5623b16a994425212e
Samuel Nogueira <samucans2004@gmail.com>
2026-03-04 00:03:41 -0300

    aaaaa

--- ajuda_ai/src/routes/route.js
+++ ajuda_ai/src/routes/route.js
@@ -2,14 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Chat from "../views/chat";
 import { ManProvider } from "../hooks/man-provider";
 
-const route = createBrowserRouter([
+const route = createBrowserRouter(
+  [
     {
-        path: "/",
-        element:
-            <ManProvider>
-                <Chat />
-            </ManProvider>
-    }
-]);
+      path: "/",
+      element: (
+        <ManProvider>
+          <Chat />
+        </ManProvider>
+      ),
+    },
+  ],
+  {
+    basename: "/ajuda-ai",
+  },
+);
 
 export default route;
